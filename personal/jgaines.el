675b31278bea4826d15a3f69491d75ae1b606f7a
John Gaines <me@jgaines.com>
2013-05-09 12:45:21 -0400

    Tweaked tuareg mode.

--- personal/jgaines.el
+++ personal/jgaines.el
@@ -80,7 +80,24 @@ initialization.
       c-tab-always-indent nil
       perl-tab-always-indent nil)
 
-;;; Use smex for M-x, Cuz smex ain't toopid!
+(when (package-installed-p 'tuareg)
+  (add-hook 'tuareg-mode-hook
+			(function (lambda ()
+						(setq tuareg-in-indent 0)
+						(setq tuareg-let-always-indent t)
+						(setq tuareg-let-indent tuareg-default-indent)
+						(setq tuareg-with-indent 0)
+						(setq tuareg-function-indent 0)
+						(setq tuareg-fun-indent 0)
+						;;(setq tuareg-parser-indent 0)
+						(setq tuareg-match-indent 0)
+						(setq tuareg-begin-indent tuareg-default-indent)
+						;;(setq tuareg-parse-indent tuareg-default-indent); .mll
+						(setq tuareg-rule-indent  tuareg-default-indent)
+						(setq tuareg-font-lock-symbols nil)
+						))))
+
+;;; Use smex for M-x
 (setq smex-save-file (concat user-emacs-directory ".smex-items"))
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
