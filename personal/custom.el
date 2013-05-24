37a4cd932199f7d51a0e2bd399a7cc413e76b820
John Gaines <me@jgaines.com>
2013-05-24 11:40:12 -0400

    Small changes to custom.el

--- personal/custom.el
+++ personal/custom.el
@@ -6,7 +6,7 @@
  '(ansi-color-names-vector ["#000000" "#ff0000" "#00ff00" "#ffff00" "#0000ff" "#dc8cc3" "#93e0e3" "#dcdccc"])
  '(ansi-term-color-vector ["#586e75" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#002b36"] t)
  '(custom-enabled-themes (quote (cyberpunk)))
- '(custom-safe-themes (quote ("1df4f61bb50f58d78e88ea75fb8ce27bac04aef1032d4ea6dafe4667ef39eb41" "c9fe7fc8e4e417fb9640933fb94c3e3a72ce06e98a5af12ac4eee8d2a9ecb1d9" "5e1d1564b6a2435a2054aa345e81c89539a72c4cad8536cfe02583e0b7d5e2fa" "f38dd27d6462c0dac285aa95ae28aeb7df7e545f8930688c18960aeaf4e807ed" "6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d1a574d57027c2bfadde6982455dfce8d27ced3ae4747c1c0313f95d23e96713" "085f323fe46529bf4526eaf7b5ae8dfb87415a68150db71a60a86c2e44329923" "72fe2de4a9971ba6578a335d26adaaa99ab6a7a6b5903d72d724789c31916c2a" "967c58175840fcea30b56f2a5a326b232d4939393bed59339d21e46cf4798ecf" "4ddc42a539280ec21ae202b6c12a4d7ce7d7af8a19e8c344b60b09f1ca1496d5" "ff7a12f1932abcdc754511b5c5c6416e769d7f1a44e64690e2c98433b18bd67e" "211bb9b24001d066a646809727efb9c9a2665c270c753aa125bace5e899cb523" "1760322f987b57884e38f4076ac586c27566a1d7ed421b67843c8c98a1501e3a" default)))
+ '(custom-safe-themes (quote ("1f31a5f247d0524ef9c051d45f72bae6045b4187ed7578a7b1f8cb8758f92b60" "1df4f61bb50f58d78e88ea75fb8ce27bac04aef1032d4ea6dafe4667ef39eb41" "c9fe7fc8e4e417fb9640933fb94c3e3a72ce06e98a5af12ac4eee8d2a9ecb1d9" "5e1d1564b6a2435a2054aa345e81c89539a72c4cad8536cfe02583e0b7d5e2fa" "f38dd27d6462c0dac285aa95ae28aeb7df7e545f8930688c18960aeaf4e807ed" "6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d1a574d57027c2bfadde6982455dfce8d27ced3ae4747c1c0313f95d23e96713" "085f323fe46529bf4526eaf7b5ae8dfb87415a68150db71a60a86c2e44329923" "72fe2de4a9971ba6578a335d26adaaa99ab6a7a6b5903d72d724789c31916c2a" "967c58175840fcea30b56f2a5a326b232d4939393bed59339d21e46cf4798ecf" "4ddc42a539280ec21ae202b6c12a4d7ce7d7af8a19e8c344b60b09f1ca1496d5" "ff7a12f1932abcdc754511b5c5c6416e769d7f1a44e64690e2c98433b18bd67e" "211bb9b24001d066a646809727efb9c9a2665c270c753aa125bace5e899cb523" "1760322f987b57884e38f4076ac586c27566a1d7ed421b67843c8c98a1501e3a" default)))
  '(fci-rule-color "#383838")
  '(frame-background-mode (quote dark))
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
@@ -17,6 +17,9 @@
  '(tab-width 4)
  '(tool-bar-mode t)
  '(transient-mark-mode t)
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3")
  '(woman-locale "en_US"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
