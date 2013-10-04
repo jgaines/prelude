c31498dabc7fff55c112f10ee1eaae560b5fbb7e
John Gaines <me@jgaines.com>
2013-10-04 17:16:27 -0400

    Remove prelude-modules.el from .gitignore

---
+++ prelude-modules.el
@@ -0,0 +1,56 @@
+;;; prelude-modules.el --- description
+
+;; Copyright (C) 2013 U-SEVERSTALCO\jgaines2
+
+;; Author: U-SEVERSTALCO\jgaines2
+;; Maintainer: U-SEVERSTALCO\jgaines2
+;; Created: Mon Sep 23 17:39:42 2013 (ignored)
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Uncomment the modules you'd like to use and restart Prelude afterwards
+
+;;; Code:
+
+(require 'prelude-c)
+;; (require 'prelude-clojure)
+(require 'prelude-coffee)
+(require 'prelude-common-lisp)
+;; (require 'prelude-css)
+(require 'prelude-emacs-lisp)
+;;(require 'prelude-erc)
+;;(require 'prelude-erlang)
+;; (require 'prelude-haskell)
+(require 'prelude-js)
+;; (require 'prelude-latex)
+(require 'prelude-lisp)
+(require 'prelude-markdown)
+;; (require 'prelude-mediawiki)
+;;(require 'prelude-org)
+(require 'prelude-perl)
+(require 'prelude-python)
+;; (require 'prelude-ruby)
+;; (require 'prelude-scala)
+(require 'prelude-scheme)
+(require 'prelude-scss)
+(require 'prelude-web)
+;; (require 'prelude-xml)
+
+(provide 'prelude-modules)
+
+;;; prelude-modules.el ends here
